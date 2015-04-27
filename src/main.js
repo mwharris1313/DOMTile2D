fee9260b5b273969ded0336ab94f462f4f70205a
Michael Harris <mwharris333@gmail.com>
2015-04-27 16:24:59 -0700

    Added screenInit(), renamed worldMap (not worldmap) to g.worldArr to prevent confusion

--- src/main.js
+++ src/main.js
@@ -12,36 +12,21 @@ g.worldmap.tileWidth = worldmap.width;
 g.worldmap.tileHeight = worldmap.height;
 g.worldmap.width = g.worldmap.tileWidth * g.tile.width;
 g.worldmap.height = g.worldmap.tileHeight * g.tile.height;
-
+g.worldArr = [];
 $(document).ready(function(){
   main();
 });
 
+// ##################################################################
 var main = function(){
   console.log('Starting main() ....');
 
   init();
-
-  var images = ['TRANSPARENT_RESERVED', '<img src="assets/blank32.jpg">', '<img src="assets/image32.jpg">'];
-  var tiles = [];
-  var MAX_TILES = g.screen.tileWidth * g.screen.tileHeight;
-  var isBlank = false;
-  for (var i=0; i<MAX_TILES; i++){
-    if (isBlank) {
-      var template = _.template(images[0]);
-    } else {
-      var template = _.template(images[1]);      
-    }
-    isBlank = !isBlank;
-    tiles.push(template(''));
-  }
-
-  for (i=0; i<MAX_TILES; i++){
-    $('#tilegroup').append(tiles[i]);
-  }
+  screenInit();
 
 };
 
+// ******************************************************************
 var init = function(){
   console.log('Starting init() ...');
   console.log('SCREEN: ', g.screen.width, g.screen.height);
@@ -52,17 +37,16 @@ var init = function(){
   var tilearray = worldmap.layers[0].data;
   console.log(tilearray);
 
-  // initial 2d worldMap array
-  var worldMap = [];
+  // initial 2d world array
   for (var i=0; i<g.worldmap.tileHeight; i++){
-    worldMap.push(Array(g.worldmap.tileWidth + 1).join('0').split(''));
+    g.worldArr.push(Array(g.worldmap.tileWidth + 1).join('0').split(''));
   }
 
   // load worldmap data into worldMap 2d array
   var tileNumber = 0;
   for (var y=0; y<g.worldmap.tileHeight; y++){
     for (var x=0; x<g.worldmap.tileWidth; x++){
-      worldMap[x][y] = tilearray[tileNumber];
+      g.worldArr[x][y] = tilearray[tileNumber];
       tileNumber++;
     }
   }
@@ -71,9 +55,54 @@ var init = function(){
   for (var y=0; y<g.worldmap.tileHeight; y++){
     var rowStr = '';
     for (var x=0; x<g.worldmap.tileWidth; x++){
-      rowStr += worldMap[x][y];
+      rowStr += g.worldArr[x][y];
     }
     console.log(rowStr);
   }
 
 };
+// ******************************************************************
+var screenInit = function(){
+  console.log('Starting screenInit() ...');
+
+  var images = ['TRANSPARENT_RESERVED', '<img src="assets/blank32.jpg">', '<img src="assets/image32.jpg">'];
+  var tiles = [];
+  var MAX_TILES = g.screen.tileWidth * g.screen.tileHeight;
+  var isBlank = false;
+
+  // test render
+  for (var y=0; y<g.screen.tileHeight; y++){
+    for (var x=0; x<g.screen.tileWidth; x++){
+      var imageIndex = g.worldArr[x][y];
+      var template = _.template(images[imageIndex]);
+      tiles.push(template(''));
+    }
+  }
+
+
+  // for (var i=0; i<MAX_TILES; i++){
+  //   if (isBlank) {
+  //     var template = _.template(images[0]);
+  //   } else {
+  //     var template = _.template(images[1]);      
+  //   }
+  //   isBlank = !isBlank;
+  //   tiles.push(template(''));
+  // }
+
+  for (i=0; i<MAX_TILES; i++){
+    $('#tilegroup').append(tiles[i]);
+  }
+
+};
+
+// ******************************************************************
+var renderScreen = function(){
+
+};
+
+// ******************************************************************
+
+
+
+
