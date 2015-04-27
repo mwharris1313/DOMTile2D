deb66825d5e3c7a601bac593489f25fb326cef85
Michael Harris <mwharris333@gmail.com>
2015-04-27 15:20:56 -0700

    Added tilegroup to hold renderable tiles, (screen element crops the overflow of the tilegroup) , Using worldmap.json attaching it to the window, placing a reference of the tile data in tilearray

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
+var g = {};
+g.screen = {};
+g.screen.width = 9;
+g.screen.height = 9;
+
 $(document).ready(function(){
   main();
 });
 
 var main = function(){
   console.log('Starting main() ....');
+  console.log('worldmap', worldmap);
+  var tilearray = worldmap.layers[0].data;
+  console.log(tilearray);
 
   var images = ['<img src="assets/blank32.jpg">', '<img src="assets/image32.jpg">'];
   var tiles = [];
-  var MAX_TILES = 64;
+  var MAX_TILES = g.screen.width * g.screen.height;
   var isBlank = false;
   for (var i=0; i<MAX_TILES; i++){
     if (isBlank) {
@@ -20,7 +28,7 @@ var main = function(){
   }
 
   for (i=0; i<MAX_TILES; i++){
-    $('body').append(tiles[i]);
+    $('#tilegroup').append(tiles[i]);
   }
 
 };
