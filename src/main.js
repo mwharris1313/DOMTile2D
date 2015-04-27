a6cfbc060a7679e2056376b5a3019cae7e0a1891
Michael Harris <mwharris333@gmail.com>
2015-04-27 13:24:50 -0700

    Added alternating tiles appended to body

--- src/main.js
+++ src/main.js
@@ -4,8 +4,23 @@ $(document).ready(function(){
 
 var main = function(){
   console.log('Starting main() ....');
-  var template = _.template('<img src="assets/image32.jpg">');
-  console.log(template({}));
 
-  $('body').append(template(''));
+  var images = ['<img src="assets/blank32.jpg">', '<img src="assets/image32.jpg">'];
+  var tiles = [];
+  var MAX_TILES = 64;
+  var isBlank = false;
+  for (var i=0; i<MAX_TILES; i++){
+    if (isBlank) {
+      var template = _.template(images[0]);
+    } else {
+      var template = _.template(images[1]);      
+    }
+    isBlank = !isBlank;
+    tiles.push(template(''));
+  }
+
+  for (i=0; i<MAX_TILES; i++){
+    $('body').append(tiles[i]);
+  }
+
 };
