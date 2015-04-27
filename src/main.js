df52a088725f2768046a944d4318f64805ee9102
Michael Harris <mwharris333@gmail.com>
2015-04-27 12:22:39 -0700

    Added underscore and jquery to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-window.onload = function(){
+$(document).ready(function(){
   main();
-};
+});
 
 var main = function(){
-  console.log('Starting main() ...');
+  console.log('Starting main() ....');
+  var template = _.template('<img src="http://tinyurl.com/n4vgcl5">');
+  console.log(template({}));
+
 };
