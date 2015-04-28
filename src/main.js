49c7952963f2de802735958394446608a0b04d5a
Michael Harris <mwharris333@gmail.com>
2015-04-28 16:10:07 -0700

    Fixed keyboard input, (remove slow start delay)

--- src/main.js
+++ src/main.js
@@ -23,7 +23,6 @@ g.adder = 2;
 g.xAdder = 6;
 g.yAdder = 2;
 
-
 g.isTransitioning = false;
 g.transitionTo = {};
 g.transitionTo.x = -1;
@@ -34,11 +33,32 @@ g.cam.x = 0;
 g.cam.y = 0;
 g.cam.xTile = 0;
 g.cam.yTile = 0;
+
+g.key = {};
+g.key.delta = 1;
+g.key.xDelta = 0;
+g.key.yDelta = 0;
+
+g.isKeyDown = false;
+g.isKeyUp = false;
+g.isKeyLeft = false;
+g.isKeyRight = false;
+
+var setKeyFalse = function(){
+  g.isKeyDown = false;
+  g.isKeyUp = false;
+  g.isKeyLeft = false;
+  g.isKeyRight = false;
+}
+
+
 g.cam.getXYTile = function(){
   var xTile = Math.floor(g.cam.x / g.tile.width);
   var yTile = Math.floor(g.cam.y / g.tile.height);
   return [xTile, yTile];
 };
+
+g.lastKeyState = '';
 
 g.tilegroup = {};
 
@@ -165,6 +185,7 @@ var screenInit = function(){
   g.tilegroup.element = $('#tilegroup')[0];
   g.tilegroup.element.addEventListener('click', onClick, false);
   document.addEventListener("keydown", onKeydown, false);
+  document.addEventListener("keyup", onKeyup, false);
 
   var xGroupOffset = g.cam.x % g.tile.width;
   var yGroupOffset = g.cam.y % g.tile.height;
@@ -227,23 +248,41 @@ var renderScreen = function(){
 
   // }
 
+  // console.log('XY1', g.cam.x,g.cam.y);
+  // if (g.key.xDelta !== 0) g.cam.x += g.key.xDelta;
+  // if (g.key.yDelta !== 0) g.cam.y += g.key.yDelta;
+  console.log('DELTA:',g.key.xDelta, g.key.yDelta);
+  // if (!isNaN(g.key.xDelta) && g.key.xDelta !== undefined && g.key.xDelta !== 0) g.cam.x += g.key.xDelta;
+  // if (!isNaN(g.key.xDelta) && g.key.yDelta !== undefined && g.key.yDelta !== 0) g.cam.y += g.key.yDelta;
+  var delta = 20;
+  if (g.isKeyDown) g.cam.y += delta;
+  if (g.isKeyUp) g.cam.y -= delta;
+  if (g.isKeyLeft) g.cam.x -= delta;
+  if (g.isKeyRight) g.cam.x += delta;
+
+  // if (!isNaN(g.key.xDelta) && g.key.xDelta !== undefined && g.key.xDelta !== 0) g.cam.x += g.key.xDelta;
+  // if (!isNaN(g.key.xDelta) && g.key.yDelta !== undefined && g.key.yDelta !== 0) g.cam.y += g.key.yDelta;
+  // console.log('XY2', g.cam.x,g.cam.y);
+
+  //console.log(g.cam.x,g.cam.y,g.key.xDelta,g.key.yDelta);
+
+  // TODO: research Robert Penner's Easing Functions...
   if (g.isTransitioning) {
+    console.log('gTransition', g.cam.x-g.transitionTo.x);
     //console.log('asdfasdfasdf');
     if (Math.abs(g.cam.x-g.transitionTo.x) <= 1) {
       g.cam.x = g.transitionTo.x;
-      g.isTransitioning = false;
+      if (g.cam.x === g.transitionTo.x && g.cam.y === g.transitionTo.y) g.isTransitioning = false;
     } else {
       g.cam.x = Math.floor(Math.sqrt(g.transitionTo.x - g.cam.x) + g.cam.x);
     }
 
     if (Math.abs(g.cam.x-g.transitionTo.x) <= 1) {
       g.cam.y = g.transitionTo.y;
-      g.isTransitioning = false;
+      if (g.cam.x === g.transitionTo.x && g.cam.y === g.transitionTo.y) g.isTransitioning = false;
     } else {
       g.cam.y = Math.floor(Math.sqrt(g.transitionTo.y - g.cam.y) + g.cam.y);
     }
-
-    console.log(g.cam.x, g.cam.y);
 
     //console.log('TESTXY',g.cam.x, g.cam.y,g.transitionTo.x, g.transitionTo.y);
   }
@@ -251,7 +290,6 @@ var renderScreen = function(){
   var xGroupOffset = g.cam.x % g.tile.width;
   var yGroupOffset = g.cam.y % g.tile.height;  g.tilegroup.element.style.left = -xGroupOffset;
   g.tilegroup.element.style.top = -yGroupOffset;
-
 
   var xyTile = g.cam.getXYTile();
   var xTile = xyTile[0];
@@ -289,27 +327,45 @@ var onClick = function(evt){
 };
 
 var onKeydown = function(evt){
+  console.log('onKeydown() ...');
+
   var up = 87;
   var down = 83;
   var left = 65;
   var right = 68;
 
+  var blankAll = function(){
+    g.key.xDelta = 0;
+    g.key.yDelta = 0;    
+  }
+
+
   if (evt.keyCode === up) {
-    console.log('up');
+    setKeyFalse();
+    g.isKeyUp = true;
+     // g.key.yDelta = g.delta;
   } else if (evt.keyCode === down) {
-    console.log('down');
-
+    setKeyFalse();
+    g.isKeyDown = true;
+    // g.key.yDelta = -g.delta;
   } else if (evt.keyCode === left) {
-    console.log('left');
-
+    setKeyFalse();
+    g.isKeyLeft = true;
+    // g.key.xDelta = g.delta;
   } else if (evt.keyCode === right) {
-    console.log('right');
-
+    setKeyFalse();
+    g.isKeyRight = true;
+    // g.key.xDelta = -g.delta;
   }
 
 }
 
-
+var onKeyup = function(evt){
+  console.log('onKeyUp() ...');
+    setKeyFalse();
+  // g.key.xDelta = 0;
+  // g.key.yDelta = 0;
+}
 //setInterval(renderScreen, 0); // using setInterval for testing, will replace with requestAnimationFrame
 // ******************************************************************
 
