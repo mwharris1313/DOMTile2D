54945bde7adde8360caeaebcdcdb42b58faee237
Michael Harris <mwharris333@gmail.com>
2015-05-25 22:18:24 -0700

    Create screen object

---
+++ src/lib/screen.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Screen = function(parms) {
+  var screen = {};
+  screen.tileWidth = parms.tileWidth;
+  screen.tileHeight = parms.tileHeight;
+  screen.width = screen.tileWidth * parms.tile.width + parms.tile.width;
+  screen.height = screen.tileHeight * parms.tile.height + parms.tile.height;
+  return screen;
+}
+
